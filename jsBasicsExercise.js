89a0a8193e72e4b0054d8e5d9b8ffc9ece2de8eb
hannahxxo <hannahantle@gmail.com>
2021-02-22 14:07:56 -0600

    Second commit with corrections and bonus

--- jsBasicsExercise.js
+++ jsBasicsExercise.js
@@ -1,4 +1,4 @@
-console.log("12");
+console.log(12);
 
 // Numbers, Strings, Booleans, Null, Undefined
 
@@ -7,18 +7,32 @@ console.log("12");
 console.log(5%4);
 console.log(5%2);
 
-console.log("I'M MARY POPPINS, Y'ALL!");
+console.log('I\'M MARY POPPINS, Y\'ALL!');
 
 console.log("Wanda" + "Vision");
 
-console.log("Hannah Antle".length);
+console.log("Hannah".length + "Antle".length);
 // 12
 
 console.log("Avengers: Endgame"[16]);
 
 // BONUS
-console.log("/");
+console.log("\\");
 
-console.log(5**4-9);
+console.log(5 ** 4 - 9);
 
-// console.log("I" - "Am" + "Iron" - "Man".length);
+console.log("I".length - "Am".length + "Iron".length - "Man".length);
+
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("HAWKEYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+var m =10;
+var c =5;
+var u = m + c;
+console.log(u);
