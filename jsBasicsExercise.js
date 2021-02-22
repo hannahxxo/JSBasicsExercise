913b69b435a15f8f5c58dc8899e5e8ab3396945c
hannahxxo <hannahantle@gmail.com>
2021-02-22 12:06:22 -0600

    First commit without completed bonus

---
+++ jsBasicsExercise.js
@@ -0,0 +1,24 @@
+console.log("12");
+
+// Numbers, Strings, Booleans, Null, Undefined
+
+// +, -, *, /, **, %
+
+console.log(5%4);
+console.log(5%2);
+
+console.log("I'M MARY POPPINS, Y'ALL!");
+
+console.log("Wanda" + "Vision");
+
+console.log("Hannah Antle".length);
+// 12
+
+console.log("Avengers: Endgame"[16]);
+
+// BONUS
+console.log("/");
+
+console.log(5**4-9);
+
+// console.log("I" - "Am" + "Iron" - "Man".length);
